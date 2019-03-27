87d6ce3149c90501112c176747911e20dfd21f35
rochford77 <ryanrochford7@gmail.com>
2019-03-27 00:25:07 -0400

    removed python-shell installation that is not necessary

--- bridge/cmdManager.js
+++ bridge/cmdManager.js
@@ -21,22 +21,7 @@ function installRequirements(){
             output.innerHTML =  stdout 
 
         }else{
-            output.innerHTML = "Error  " + stderr
-        }
-
-        div.appendChild(output)
-        updateScroll(div)
-    });
-
-    exec('npm install python-shell', function(error, stdout, stderr) {
-        var div = document.getElementById("output-container")
-        var output = document.createElement("output")
-        output.setAttribute('class', 'output-line-item')
-
-        if(stderr == ""){
             output.innerHTML =  stdout 
-
-        }else{
             output.innerHTML = "Error  " + stderr
         }
 
@@ -52,18 +37,23 @@ function checkPyVersion(){
         var div = document.getElementById("output-container")
         var output = document.createElement("output")
         output.setAttribute('class', 'output-line-item')
-       
 
         if(stderr == ""){
             output.innerHTML = "Your Python version is: " + stdout + ". ATTN: Python 3.6.2 or higher required"
-
+            div.appendChild(output)
         }else{
             output.innerHTML = "Error getting Python version " + stderr + ". ATTN: Python 3.6.2 or higher required"
+            var help1 = document.createElement("output")
+            help1.setAttribute('class', 'output-line-item')
+            help1.innerHTML = "Please visit https://www.python.org/downloads/ to install the latest version."
+            var help2 = document.createElement("output")
+            help2.setAttribute('class', 'output-line-item')
+            help2.innerHTML = "DO NOT FORGET TO CHECK THE ADD TO PATH BUTTON SEE: https://imgur.com/UWcWf74"
+            div.appendChild(output)
+            div.appendChild(help1)
+            div.appendChild(help2)
         }
-
-        div.appendChild(output)
         updateScroll(div)
-
     });
 }
 
